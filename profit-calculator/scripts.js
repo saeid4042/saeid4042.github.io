a38c76b8921f19a499b775eb465bf1f9f1f3d5db
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-04-28 22:17:17 +0330

    master: .

--- profit-calculator/scripts.js
+++ profit-calculator/scripts.js
@@ -41,44 +41,47 @@ function commafy( num ) {
 }
 
 const update = () => {
+    amountPreview.innerText = amount?.value;
+    entryPricePreview.innerText = entryPrice?.value;
+    exitPricePreview.innerText = exitPrice?.value;
+    stopLossPreview.innerText = stopLoss?.value;
+    levPreview.innerText = lev?.value;
+    dollarPreview.innerText = dollar?.value;
+    liqPreview.innerText = liq?.value;
 
-    amountPreview.innerText = amount.value;
-    entryPricePreview.innerText = entryPrice.value;
-    exitPricePreview.innerText = exitPrice.value;
-    stopLossPreview.innerText = stopLoss.value;
-    levPreview.innerText = lev.value;
-    dollarPreview.innerText = dollar.value;
-    liqPreview.innerText = liq.value;
+    const _diff = exitPrice?.value - entryPrice?.value
+    const _diff2 = entryPrice?.value - stopLoss?.value
+    const _gross = amount?.value * lev?.value
 
-    const _diff = exitPrice.value - entryPrice.value
-    const _diff2 = entryPrice.value - stopLoss.value
-    const _gross = amount.value * lev.value
-    const _RR = totalProfit.value / totalLoss.value
+    console.log(totalProfit?.value);
+    console.log(totalLoss?.value);
 
-    const _lossPercentage = (_diff2) * 100 / entryPrice.value
-    const _totalLoss = _lossPercentage * amount.value / 100
-    const _profitPercentage = (_diff) * 100 / entryPrice.value
-    const _totalProfit = _profitPercentage * amount.value / 100
+    const _lossPercentage = (_diff2) * 100 / entryPrice?.value
+    const _totalLoss = _lossPercentage * amount?.value / 100
+    const _profitPercentage = (_diff) * 100 / entryPrice?.value
+    const _totalProfit = _profitPercentage * amount?.value / 100
 
-    const _lossPercentageLev = _lossPercentage * lev.value
-    const _totalLossLev = _totalLoss * lev.value
-    const _profitPercentageLev = _profitPercentage * lev.value
-    const _totalProfitLev = _totalProfit * lev.value
-    const _totalLossLevInToman = _totalLossLev * dollar.value
-    const _totalProfitLevInToman = _totalProfitLev * dollar.value
+    const _lossPercentageLev = _lossPercentage * lev?.value
+    const _totalLossLev = _totalLoss * lev?.value
+    const _profitPercentageLev = _profitPercentage * lev?.value
+    const _totalProfitLev = _totalProfit * lev?.value
+    const _totalLossLevInToman = _totalLossLev * dollar?.value
+    const _totalProfitLevInToman = _totalProfitLev * dollar?.value
+    const _RR = _totalLoss / _totalProfit
+
     
     diff.innerText = commafy(_diff);
     diff2.innerText = commafy(_diff2);
     gross.innerText = commafy(_gross);
     
     lossPercentage.innerText = commafy(_lossPercentage);
-    totalLoss.innerText = commafy(_totalLoss);
+    // totalLoss.innerText = commafy(_totalLoss);
     profitPercentage.innerText = commafy(_profitPercentage);
-    totalProfit.innerText = commafy(_totalProfit);
+    // totalProfit.innerText = commafy(_totalProfit);
     
-    lossPercentageLev.innerText = commafy(_lossPercentageLev);
+    // lossPercentageLev.innerText = commafy(_lossPercentageLev);
     totalLossLev.innerText = commafy(_totalLossLev);
-    profitPercentageLev.innerText = commafy(_profitPercentageLev);
+    // profitPercentageLev.innerText = commafy(_profitPercentageLev);
     totalProfitLev.innerText = commafy(_totalProfitLev);
 
     totalLossLevInToman.innerText = commafy(_totalLossLevInToman);
